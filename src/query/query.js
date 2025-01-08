2c1e8c947039e8a5da8fd77b060668a3ae3666c1
NgurahP <ngurahputra2425@gmail.com>
2025-01-08 09:18:32 +0800

    finishing the navbar

--- src/query/query.js
+++ src/query/query.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
     $("header").load("./src/public/Navbar.html")
     $(window).scroll(function() {
         if ($(this).scrollTop() > 10) {
-            navbar.removeClass('bg-transparent block').addClass('bg-indigo-300 fixed');
+            navbar.removeClass('bg-transparent block py-[0.75%]').addClass('bg-indigo-300 fixed');
+            navbar.animate({
+                padding: "1% 0 1% 0",
+            })
         } else {
-            navbar.removeClass('bg-indigo-300 fixed').addClass('bg-transparent block');
+            navbar.removeClass('bg-indigo-300 fixed py-[1%]').addClass('bg-transparent block py-[0.75%]');
         }
     });
 });
